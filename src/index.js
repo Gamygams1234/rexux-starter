df5ae79537a11f216817c517b6afc4d28fb8a8b4
Gamygams1234 <gamyburgos@gmail.com>
2020-07-09 00:17:55 -0700

    Making the counter and the store

--- src/index.js
+++ src/index.js
@@ -16,8 +16,24 @@ const increment = () => {
   };
 };
 
+const decrement = () => {
+  return {
+    type: "DECREMENT",
+  };
+};
+
 // reducer
 
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state + 1;
+  }
+};
+
+let store = createStore(counter);
 // dispatch
 ReactDOM.render(
   <React.StrictMode>
