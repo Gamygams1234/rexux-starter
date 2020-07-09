ccb356743969330c1e7d04d5730d9dc95fb0c50d
Gamygams1234 <gamyburgos@gmail.com>
2020-07-09 00:51:48 -0700

    Setting up two reducers

--- src/index.js
+++ src/index.js
@@ -3,45 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore } from "redux";
 
 // store Globalized state
 
 // action   "this is like saying that I am hungry, butn not doing anything yet"
 // function that returns an object
-
-const increment = () => {
-  return {
-    type: "INCREMENT",
-  };
-};
-
-const decrement = () => {
-  return {
-    type: "DECREMENT",
-  };
-};
-
-// this is our counter variable or reducer
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-  }
-};
-
-let store = createStore(counter);
-
-// display it in the console
-
-// dispatch
-store.subscribe(() => console.log(store.getState()));
-store.dispatch(increment());
-
-store.dispatch(decrement());
-store.dispatch(decrement());
 
 ReactDOM.render(
   <React.StrictMode>
